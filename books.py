f8988dd140fcac8dfa0645f88ff179714c065aa4
Aashish Karki <0133asis@gmail.com>
2015-07-14 11:43:19 -0400

    Using cmd module

--- books.py
+++ books.py
@@ -1,1 +1,20 @@
-def read:
+from cmd import Cmd
+
+class  booksPrompt(Cmd):
+
+    def do_hello(self, args):
+        if len(args) == 0:
+            name = 'stranger'
+        else:
+            name = args
+        print 'Hello, %s' % name
+    def do_quit(self, args):
+        print "Quitting"
+        raise SystemExit
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) > 1:
+        booksPrompt().onecmd(' '.join(sys.argv[1:]))
+    else:
+        print "Help"
